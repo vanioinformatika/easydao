884080b9f76c09d14630dafc9dd41e9aaaf996d2
István Pató <istvan.pato@gmail.com>
2013-12-25 11:07:20 +0100

    remove import

--- src/main/java/hu/vanio/easydao/modelbuilder/ModelBuilder.java
+++ src/main/java/hu/vanio/easydao/modelbuilder/ModelBuilder.java
@@ -32,7 +32,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map.Entry;
 
 /**
  * Abstract class for model builders.
