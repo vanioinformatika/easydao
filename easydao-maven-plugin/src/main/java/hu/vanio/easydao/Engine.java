339568abf1bb52ec3094cf8382d57305ba24d0e7
István Pató <istvan.pato@gmail.com>
2015-11-21 16:39:02 +0100

    drop easydao-core dependency: maven plugin copies all neccessary interfaces and classes into generated source code directory

--- easydao-maven-plugin/src/main/java/hu/vanio/easydao/Engine.java
+++ easydao-maven-plugin/src/main/java/hu/vanio/easydao/Engine.java
@@ -50,16 +50,17 @@ import hu.vanio.easydao.modelbuilder.ModelBuilder;
 import hu.vanio.easydao.modelbuilder.Oracle10ModelBuilderConfig;
 import hu.vanio.easydao.modelbuilder.Oracle11ModelBuilderConfig;
 import hu.vanio.easydao.modelbuilder.PostgreSql9ModelBuilderConfig;
+import java.util.Arrays;
 
 /**
  * Generate domain object and Dao classes from database based on configuration.
- * 
+ *
  * @author Istvan Pato <istvan.pato@vanio.hu>
  */
 public class Engine {
 
     static final private String fileSeparator = System.getProperty("file.separator");
-    
+
     /** Engine configuration */
     private EngineConfiguration engineConfiguration;
     /** Database model builder configuration */
@@ -68,10 +69,10 @@ public class Engine {
     private Configuration freemarkerConfig;
     /** Handles localised messages */
     private LocalisedMessages messages;
-    
+
     /**
      * Constructs a new instance with the specified configuration
-     * 
+     *
      * @param engineConfiguration Engine configuration
      * @throws java.sql.SQLException If registering the JDBC driver fails
      */
@@ -123,6 +124,8 @@ public class Engine {
         // create java source codes: model and dao
         generateModelClasses();
         generateDaoClasses();
+        // copy static java classes
+        copyInterfacesAndClasses();
     }
 
     private void generateMetadataFile() throws IOException, TemplateException {
@@ -209,6 +212,78 @@ public class Engine {
     }
 
     /**
+     * Copy core interfaces and classes.
+     * @throws IOException
+     * @throws TemplateException
+     */
+    private void copyInterfacesAndClasses() throws IOException, TemplateException {
+        Configuration javaCopyConfig = new Configuration();
+        javaCopyConfig.setClassForTemplateLoading(Engine.class, "/hu/vanio/easydao/core");
+        javaCopyConfig.setObjectWrapper(new DefaultObjectWrapper());
+        javaCopyConfig.setDefaultEncoding("UTF-8");
+        javaCopyConfig.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+        javaCopyConfig.setIncompatibleImprovements(new Version(2, 3, 23));
+
+        // copy core dir
+        final Path dir = Paths.get(engineConfiguration.getGeneratedSourcePath(),
+                "hu", "vanio", "easydao", "core");
+        Files.createDirectories(dir);
+        if (!engineConfiguration.isSilent()) {
+            System.out.println("\nWrite interfaces and classes into " + dir.toAbsolutePath());
+        }
+
+        final List<String> staticFileList = Arrays.asList(
+                "Dao.java",
+                "DaoComp.java",
+                "DaoExt.java",
+                "Model.java",
+                "Service.java");
+        for (String file : staticFileList) {
+            Template temp = javaCopyConfig.getTemplate(file);
+            this.copy(dir, temp);
+        }
+
+        // copy core/postgresql dir
+        final String subdir = "postgresql/";
+
+        Files.createDirectories(Paths.get(engineConfiguration.getGeneratedSourcePath(),
+                "hu", "vanio", "easydao", "core", subdir));
+
+        if (!engineConfiguration.isSilent()) {
+            System.out.println("\nWrite interfaces and classes into " + dir.toAbsolutePath());
+        }
+
+        final List<String> staticFileList2 = Arrays.asList(
+                "PostgreSqlArrayFactory.java",
+                "PostgreSqlDoubleArray.java",
+                "PostgreSqlFloatArray.java",
+                "PostgreSqlInt4Array.java",
+                "PostgreSqlTextArray.java");
+        for (String file : staticFileList2) {
+            Template temp = javaCopyConfig.getTemplate(subdir + file);
+            this.copy(dir, temp);
+        }
+
+    }
+
+    /**
+     * Copy template file to directory.
+     * @param dir
+     * @param temp
+     * @throws IOException
+     * @throws TemplateException
+     */
+    protected void copy(Path dir, Template temp) throws IOException, TemplateException {
+        Map<String, Object> m = new HashMap<>();
+        m.put("e", engineConfiguration);
+        try (Writer fileWriter = new OutputStreamWriter(
+                new FileOutputStream(
+                        new File(dir.toAbsolutePath().toString() + fileSeparator + temp.getName())), engineConfiguration.getEncoding())) {
+            temp.process(m, fileWriter);
+        }
+    }
+
+    /**
      * @see http://freemarker.org/docs/pgui_quickstart_createconfiguration.html
      * @return Freemarker configuration
      */
@@ -218,7 +293,7 @@ public class Engine {
         freemarkerConfig.setObjectWrapper(new DefaultObjectWrapper());
         freemarkerConfig.setDefaultEncoding("UTF-8");
         freemarkerConfig.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
-        freemarkerConfig.setIncompatibleImprovements(new Version(2, 3, 20));
+        freemarkerConfig.setIncompatibleImprovements(new Version(2, 3, 23));
     }
 
     /**
