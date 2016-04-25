91de1b24501293ec6e71bcc2414a4a85ae921b24
István Pató <istvan.pato@gmail.com>
2016-04-25 18:49:14 +0200

    start docker sh script update

--- easydao-samples/creating-sampledb/run-container.sh
+++ easydao-samples/creating-sampledb/run-container.sh
@@ -1,4 +1,4 @@
-docker run --name sampledb -e POSTGRES_PASSWORD=sample -d easydao-sampledb
+docker run -d -p 15432:5432 --name sampledb -e POSTGRES_PASSWORD=sample easydao-sampledb
 
-ip=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' sampledb) && export DB_IP_ADDRESS=$ip
-
+#ip=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' sampledb) && export DB_IP_ADDRESS=$ip
+export DB_IP_ADDRESS=127.0.0.1:15432
