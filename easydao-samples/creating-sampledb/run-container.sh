f925c746c38d91ba0cd88f67e8782a206a603add
Pato Istvan <istvan.pato@vanio.hu>
2016-08-02 14:59:13 +0200

    change port number (searching TravisCI issue)

--- easydao-samples/creating-sampledb/run-container.sh
+++ easydao-samples/creating-sampledb/run-container.sh
@@ -1,4 +1,4 @@
-docker run -d -p 15432:5432 --name sampledb -e POSTGRES_PASSWORD=sample easydao-sampledb
+docker run -d -p 5432:5432 --name sampledb -e POSTGRES_PASSWORD=sample easydao-sampledb
 
 #ip=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' sampledb) && export DB_IP_ADDRESS=$ip
-export DB_IP_ADDRESS=0.0.0.0:15432
+export DB_IP_ADDRESS=0.0.0.0:5432
