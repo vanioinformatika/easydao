5289c62a30934ea6c689d68c5d5379cd7ffc3e64
Pato Istvan <istvan.pato@vanio.hu>
2016-08-02 14:45:26 +0200

    IP 0.0.0.0 from 127.0.0.1

--- easydao-samples/creating-sampledb/run-container.sh
+++ easydao-samples/creating-sampledb/run-container.sh
@@ -1,4 +1,4 @@
 docker run -d -p 15432:5432 --name sampledb -e POSTGRES_PASSWORD=sample easydao-sampledb
 
 #ip=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' sampledb) && export DB_IP_ADDRESS=$ip
-export DB_IP_ADDRESS=127.0.0.1:15432
+export DB_IP_ADDRESS=0.0.0.0:15432
